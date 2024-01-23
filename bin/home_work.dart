2e94bea84a0c213f3a03d85c98940b109c04a186
Aidin30 <azamatuuluaidin@gmail.com>
2024-01-23 19:54:26 +0600

    first commit

--- bin/home_work.dart
+++ bin/home_work.dart
@@ -1,5 +1,40 @@
-import 'package:home_work/home_work.dart' as home_work;
+void main() {
+String helloDart = ('Hello,Dart!');
+ print (helloDart.runtimeType);
 
-void main(List<String> arguments) {
-  print('Hello world: ${home_work.calculate()}!');
+ String myDart = ('Dart programming language');
+ String dart = 'dart';
+ print(myDart.contains(dart));
+
+
+  String dartIsFun = ('dartIsFun');
+  print (dartIsFun.toUpperCase());
+
+  String dartIsCool = ('dartIsCool');
+  print (dartIsCool.toLowerCase());
+   
+   String Hello = ('Hello');
+   String Dart = ('Dart');
+   String love = '$Dart' '$Hello';
+   print(love);
+
+   String spacesBaby = ('    ');
+   bool isEmpty = spacesBaby.trim().isEmpty;
+   print (isEmpty);
+
+   String DartProgramming = ('DartProgramming');
+   print (DartProgramming.length);
+           String trimMe = (" Trim me! " );
+   String trimmedString = trimMe.trim();
+   print (trimmedString);
+    String replaceSpaces = ('replace spaces with underscores');
+String replace= replaceSpaces.replaceAll(' ', '_');
+print(replace);
+print ('   ');
+ String spacesOnly = "    ";
+  bool containsOnlySpaces = spacesOnly.trim().isEmpty;
+  print('$containsOnlySpaces');
+
+
+
 }
